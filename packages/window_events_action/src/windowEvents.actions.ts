166d3ce4aeb4885337d4418d72d8ec44c7d53b89
jcarbou <jcarbou@gmail.com>
2025-10-11 12:25:15 +0200

    bug fix

--- packages/window_events_action/src/windowEvents.actions.ts
+++ packages/window_events_action/src/windowEvents.actions.ts
@@ -8,21 +8,24 @@ type Options = {
     windoworientationchange?: WindowEventHandler
 }
 
-/** Dispatch event on window resize or windoworientationchange */
-export function onMountWindowEvents({ resize, windoworientationchange, scroll }: Options): ActionReturn<Options> {
+/** Dispatch event on window resize, scroll or windoworientationchange using svelte action */
+export function windowEvents(node: HTMLElement, options: Options): ActionReturn<Options> {
+    const destroy = onMountWindowEvents(options)
+
+    return {
+        destroy
+    }
+}
+
+/** Dispatch event on window resize, scroll or windoworientationchange using svelte onMount */
+export function onMountWindowEvents({ resize, windoworientationchange, scroll }: Options): () => void {
     resize && window.addEventListener('resize', resize, true)
     scroll && window.addEventListener('scroll', scroll, true)
     windoworientationchange && window.addEventListener('windoworientationchange', windoworientationchange, true)
 
-    return {
-        destroy() {
-            resize && window.removeEventListener('resize', resize, true)
-            scroll && window.removeEventListener('scroll', scroll, true)
-            windoworientationchange && window.removeEventListener('windoworientationchange', windoworientationchange, true)
-        }
+    return () => {
+        resize && window.removeEventListener('resize', resize, true)
+        scroll && window.removeEventListener('scroll', scroll, true)
+        windoworientationchange && window.removeEventListener('windoworientationchange', windoworientationchange, true)
     }
-}
-
-export function windowEvents(node: HTMLElement, options: Options): ActionReturn<Options> {
-    return onMountWindowEvents(options)
 }
