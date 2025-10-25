fe46ae1034838bf61e267764710fa652a475cb69
jcarbou <jcarbou@gmail.com>
2025-10-25 17:21:01 +0200

    Remove temp files

--- packages/test/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as JazzSvelteTestContext } from './JazzSvelteTestContext.svelte'
-export * from './utils'
--- packages/test/src/utils.types.ts
+++
@@ -1,6 +0,0 @@
-import { Component, ComponentProps, SvelteComponent } from 'svelte'
-
-export type ComponentToTestProps<CMP extends Component> = ComponentProps<CMP> & { 'data-tested-element': true }
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type TestedComponent = SvelteComponent<Record<string, any>, any, any>
